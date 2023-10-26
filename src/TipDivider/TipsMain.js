c11be36cf97e4caea6f23b9aa5450c6ff56ae28b
LiamD21 <58191445+LiamD21@users.noreply.github.com>
2023-10-26 16:42:19 -0600

    Using html form to get values from text fields into js

--- src/TipDivider/TipsMain.js
+++ src/TipDivider/TipsMain.js
@@ -1,3 +1,27 @@
-function getInput(){
-    let x = document.getElementById("numPeople").value;
+function getNumPeople(form){
+    return form.numPeople.value;
+}
+
+function getTotalCash(form){
+    return form.numCash.value;
+}
+
+function getTotalCard(form){
+    return form.numCard.value;
+}
+
+function getHours(form){
+    let hoursArray = form.numHours.value;
+    let floatHoursArr = [];
+    for (let i = 0; i < hoursArray.length; i++){
+        floatHoursArr[i] = parseFloat(hoursArray[i]);
+    }
+    return floatHoursArr;
+}
+
+function main(form){
+    let numPeople = getNumPeople(form);
+    let totalCash = getTotalCash(form);
+    let totalCard = getTotalCard(form);
+    let hours = getHours(form);
 }
