6f0e644b471e2ffc681034478b1d84a0448c9c53
LiamD21 <58191445+LiamD21@users.noreply.github.com>
2023-10-27 09:11:27 -0600

    Refactoring and changing the idea of my js project because of limitations.

---
+++ src/TipDivider/jsTests.js
@@ -0,0 +1,17 @@
+function getNumPeople(form){
+    return form.numPeople.value;
+}
+
+function getTotalCash(form){
+    return form.numCash.value;
+}
+
+function getTotalCard(form){
+    return form.numCard.value;
+}
+
+function main(form){
+    // let numPeople = getNumPeople(form);
+    // let totalCash = getTotalCash(form);
+    // let totalCard = getTotalCard(form);
+}
