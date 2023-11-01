ebfd668984f9e7e1885a27cdaa578988b81cc2a0
LiamD21 <58191445+LiamD21@users.noreply.github.com>
2023-11-01 15:04:13 -0600

    Added button class to make creating buttons on screen easier

--- src/ConnectFour/cFourMenu.js
+++ src/ConnectFour/cFourMenu.js
@@ -1,7 +1,9 @@
+// Importing classes
+const Button = require("./modules/buttonClass");
+
 // Global Variables
 let canvas;
 let ctx;
-let overStartButton;
 
 function initializeCanvas() {
     canvas = document.getElementById("canvas");
@@ -13,14 +15,13 @@ function initializeCanvas() {
 function addText(){
     ctx.font = "50px Comic Sans MS";
     ctx.textAlign = "center";
-    ctx.fillStyle = "#7b00ff"
+    ctx.fillStyle = "#7b00ff";
     ctx.fillText("Connect Four", canvas.width/2, canvas.height/4);
 }
 
 function drawMenuButtons(){
-    ctx.fillStyle = "#888888";
-    ctx.fillRect(canvas.width/2 - 100, canvas.height*2/3, 200, 100);
-    ctx.strokeRect(canvas.width/2 - 100, canvas.height*2/3, 200, 100);
+    let menuButton = new Button(ctx, canvas.width/2 - 100, canvas.height*2/3, 100, 200, "PLAY", "#888888");
+    menuButton.draw();
 }
 
 function main(){
---
+++ src/ConnectFour/modules/buttonClass.js
@@ -0,0 +1,27 @@
+class Button {
+    constructor(ctx, x, y, height, width, text, color) {
+        this.ctx = ctx;
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width;
+        this.text = text;
+        this.color = color;
+    }
+
+    getCenterXY(){
+        return [this.x + (this.width/2), this.y + (this.height/2)];
+    }
+
+    draw(){
+        this.ctx.fillStyle = this.color;
+        this.ctx.fillRect(this.x, this.y, this.width, this.height);
+        this.ctx.fillStyle = "#000000";
+        this.ctx.textAlign = "center";
+        this.ctx.font = "10px";
+        this.ctx.lineWidth = 10;
+        this.ctx.strokeRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+module.exports = Button;
