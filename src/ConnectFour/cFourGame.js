620331b218dc6f9adb4b4fa42705ece1afa4f236
LiamD21 <58191445+LiamD21@users.noreply.github.com>
2023-11-01 21:08:32 -0600

    Drew all of the empty cells on the game board

--- src/ConnectFour/cFourGame.js
+++ src/ConnectFour/cFourGame.js
@@ -5,9 +5,26 @@ let ctx, canvas;
  * Draws a game board on the canvas
  */
 function drawBoard(cellSize){
+    let boardTop = cellSize;
+    let boardLeft = canvas.width/2-(cellSize*3.5);
+
     // draw the main blue square for the board
     ctx.fillStyle = "#1138f6"
-    ctx.fillRect(canvas.width/2-(cellSize*3.5), cellSize, cellSize*7, cellSize*6);
+    ctx.fillRect(boardLeft, boardTop, cellSize*7, cellSize*6);
+
+    // draw all the cells
+    for (let i = 0; i < 7; i++) {
+        for (let j = 0; j < 6; j++) {
+            drawCircle("white", boardLeft + (i * cellSize) + cellSize / 2, boardTop + (j * cellSize) + cellSize / 2, cellSize / 2 - 5);
+        }
+    }
+}
+
+function drawCircle(color, x, y, rad){
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(x, y, rad, 0, Math.PI*2, false);
+    ctx.fill();
 }
 
 /**
