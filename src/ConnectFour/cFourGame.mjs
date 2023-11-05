fdebb18c70fa1117586fce83589e7897a5740f6a
LiamD21 <58191445+LiamD21@users.noreply.github.com>
2023-11-04 18:20:54 -0600

    Implemented dragging the next piece around the board

--- src/ConnectFour/cFourGame.mjs
+++ src/ConnectFour/cFourGame.mjs
@@ -49,6 +49,7 @@ function addNextPiece(height, context, radius){
     // if the state is equal to placed, we need to add another piece
     if(currentState === states.PLACED){
         nextPiece = new NextPiece("red", radius, height, context);
+        currentState = states.READY;
     }
 }
 
@@ -58,7 +59,10 @@ function addNextPiece(height, context, radius){
  * @param event
  */
 function handleMousePress(event) {
-
+    // if the state is ready, and we are over the object, enter selected
+    if (currentState === states.READY && overNextPiece){
+        currentState = states.SELECTED;
+    }
 }
 
 /**
@@ -67,7 +71,10 @@ function handleMousePress(event) {
  * @param event
  */
 function handleMouseRelease(event){
-
+    // if the state is currently selected, go back to placed
+    if (currentState === states.SELECTED){
+        currentState = states.PLACED;
+    }
 }
 
 /**
@@ -75,17 +82,22 @@ function handleMouseRelease(event){
  * if the item to place is selected, drag it with the mouse
  * @param event
  */
-// function handleMouseDragged(event){
-//     // check if mouse is hovering over the piece to drag
-//     // if () {
-//     //     // check if the mouse was just off the piece
-//     //     if (!overNextPiece) {
-//     //         overNextPiece = true;
-//     //     }
-//     // }
-//
-//     // if mouse is not over next piece, but it just was over the next piece
-//     else if (overNextPiece){
-//         overNextPiece = false;
-//     }
-// }
+function handleMouseDragged(event){
+    // check if state is selected, then we must drag the piece
+    if (currentState === states.SELECTED){
+        nextPiece.move(event.clientX, event.clientY);
+    }
+
+    // check if mouse is hovering over the piece to drag
+    if (nextPiece.checkIntersect(event.clientX, event.clientY)) {
+        // check if the mouse was just off the piece
+        if (!overNextPiece) {
+            overNextPiece = true;
+        }
+    }
+
+    // if mouse is not over next piece, but it just was over the next piece
+    else if (overNextPiece){
+        overNextPiece = false;
+    }
+}
--- src/ConnectFour/modules/nextPieceClass.js
+++ src/ConnectFour/modules/nextPieceClass.js
@@ -4,6 +4,8 @@ export class NextPiece{
     #radius
     #color
     #context
+    #initialCenterX
+    #initialCenterY
 
     constructor(color, radius, height, ctx) {
         this.#color = color;
@@ -12,20 +14,45 @@ export class NextPiece{
         this.#centerX = 3 * this.#radius;
         this.#centerY = height - (3 * this.#radius);
 
-        this.#draw();
+        this.#initialCenterX = this.#centerX;
+        this.#initialCenterY = this.#centerY;
+
+        this.#draw(this.#centerX, this.#centerY);
     }
 
     /**
      * draws a circle at the class's coordinates and radius
      */
-    #draw(){
+    #draw(x, y){
         this.#context.fillStyle = this.#color;
         this.#context.beginPath();
-        this.#context.arc(this.#centerX, this.#centerY, this.#radius, 0, Math.PI*2, false);
+        this.#context.arc(x, y, this.#radius, 0, Math.PI*2, false);
         this.#context.fill();
     }
 
-    move(){
+    move(newX, newY){
+        this.#context.clearRect(this.#centerX - this.#radius, this.#centerY - this.#radius, this.#radius*2, this.#radius*2);
+        this.#draw(newX, newY);
+        this.#centerY = newY;
+        this.#centerX = newX;
+    }
 
+    reset(){
+
+    }
+
+    /**
+     * checks to see if a given pair of points is inside or outside the circle
+     * @param x the given x coordinate
+     * @param y the given y coordinate
+     * @return {boolean} true if the given x, y is inside the circle
+     */
+    checkIntersect(x, y){
+        let dx = this.#centerX - x;
+        let dy = this.#centerY - y;
+
+        let hyp = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+
+        return hyp <= this.#radius;
     }
 }
