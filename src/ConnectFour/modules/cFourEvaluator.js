ea2844382d134a68fe37195be1202e6655b35265
LiamD21 <58191445+LiamD21@users.noreply.github.com>
2023-12-07 19:23:16 -0600

    Implemented checks for any groups of 4 which already contain 1, 2, or 3 pieces. These are weighted higher in the evaluation algorithm

--- src/ConnectFour/modules/cFourEvaluator.js
+++ src/ConnectFour/modules/cFourEvaluator.js
@@ -21,20 +21,60 @@ export function getEvalScore(){
 }
 
 function evaluatePosition(){
+    let scores = checkPossibilities();
+    // if we have any wins, the game is over
+    if (scores[8] === 1){
+        return 10;
+    }
+    else if (scores[9] === 1){
+        return -10;
+    }
+
+    // if no wins, continue with the evaluation
+    let p1TotalOptions = scores[0];
+    let p2TotalOptions = scores[1];
+
+    // remove all options from total moves which also appear as singles, doubles, or triples
+    p1TotalOptions = p1TotalOptions - scores[2] - scores[4] - scores[6];
+    p2TotalOptions = p2TotalOptions - scores[3] - scores[5] - scores[7];
+
+    // create overall scores
+    // weights: empty options = 1, singles = 2, doubles = 4, triples = 10
+    let p1TotalWt = p1TotalOptions + (scores[2] * 2) + (scores[4] * 4) + (scores[6] * 10);
+    let p2TotalWt = p2TotalOptions + (scores[3] * 2) + (scores[5] * 4) + (scores[7] * 10);
+
+    // return the weight as a score out of 10. negative means that p2 is ahead, positive means that p1 is ahead
+    return (p1TotalWt-p2TotalWt)/10;
+}
+
+function checkPossibilities(){
     let p1Score = 0;
     let p2Score = 0;
+    let p1Singles = 0;
+    let p2Singles = 0;
+    let p1Doubles = 0;
+    let p2Doubles = 0;
+    let p1Triples = 0;
+    let p2Triples = 0;
+    let p1Win = 0;
+    let p2Win = 0;
 
-    let p1ColStreak;
-    let p2ColStreak;
+    let p1ColStreak, p1ColLastFour;
+    let p2ColStreak, p2ColLastFour;
 
     // find all possible ways that each player can make 4 in the current position
 
     let p1RowStreaks = [0, 0, 0, 0, 0, 0];
     let p2RowStreaks = [0, 0, 0, 0, 0, 0];
+    let emptyLastFour = [[-1, -1, -1, -1], [-1, -1, -1, -1], [-1, -1, -1, -1], [-1, -1, -1, -1], [-1, -1, -1, -1], [-1, -1, -1, -1]];
+    let p1RowLastFour = emptyLastFour;
+    let p2RowLastFour = emptyLastFour;
     // check vertical and horizontal 4s that can be made
     for (let col = 0; col < 7; col++){
         p1ColStreak = 0;
+        p1ColLastFour = [-1, -1, -1, -1];
         p2ColStreak = 0;
+        p2ColLastFour = [-1, -1, -1, -1];
         for(let row = 0; row < 6; row++){
             // check the current element
             if (gameBoard[row][col] === 0){
@@ -56,30 +96,64 @@ function evaluatePosition(){
                 p2RowStreaks[row] ++;
             }
 
-            // if we have a vertical streak of 4 or more, add one way to get four
+            // update the lists of last four on the current streak
+            for (let i = 3; i >= 1; i--){
+                p1ColLastFour[i] = p1ColLastFour[i-1];
+                p2ColLastFour[i] = p2ColLastFour[i-1];
+                p1RowLastFour[row][i] = p1RowLastFour[row][i-1];
+                p2RowLastFour[row][i] = p2RowLastFour[row][i-1];
+            }
+            p1ColLastFour[0] = gameBoard[row][col];
+            p2ColLastFour[0] = gameBoard[row][col];
+            p1RowLastFour[row][0] = gameBoard[row][col];
+            p2RowLastFour[row][0] = gameBoard[row][col];
+
+            let counters;
+            // if we have a vertical streak of 4 or more, add one way to get four and check for any pieces already there
             if (p1ColStreak >= 4){
                 p1Score ++;
+                counters = incrementScores(countLastFour(1, p1ColLastFour), p1Singles, p1Doubles, p1Triples, p1Win);
+                p1Singles = counters[0];
+                p1Doubles = counters[1];
+                p1Triples = counters[2];
+                p1Win = counters[3];
             }
             if (p2ColStreak >= 4){
-                p2Score++;
+                p2Score ++;
+                counters = incrementScores(countLastFour(1, p2ColLastFour), p2Singles, p2Doubles, p2Triples, p2Win);
+                p2Singles = counters[0];
+                p2Doubles = counters[1];
+                p2Triples = counters[2];
+                p2Win = counters[3];
             }
 
-            // if we have a horizontal streak of 4 or more, add one way to get four
+            // if we have a horizontal streak of 4 or more, add one way to get four and check for pieces already there
             if (p1RowStreaks[row] >= 4){
                 p1Score ++;
+                counters = incrementScores(countLastFour(1, p1RowLastFour), p1Singles, p1Doubles, p1Triples, p1Win);
+                p1Singles = counters[0];
+                p1Doubles = counters[1];
+                p1Triples = counters[2];
+                p1Win = counters[3];
             }
             if (p2RowStreaks[row] >= 4){
-                p2Score++;
+                p2Score ++;
+                counters = incrementScores(countLastFour(1, p2RowLastFour), p2Singles, p2Doubles, p2Triples, p2Win);
+                p2Singles = counters[0];
+                p2Doubles = counters[1];
+                p2Triples = counters[2];
+                p2Win = counters[3];
             }
         }
     }
 
     // Check ways to make 4 on the diagonals
-    let p1DiagStreak;
-    let p2DiagStreak;
+    let p1DiagStreak, p2DiagStreak, p1DiagLastFour, p2DiagLastFour;
     for (let i = 0; i < 12; i++){
         p1DiagStreak = 0;
         p2DiagStreak = 0;
+        p1DiagLastFour = [-1, -1, -1, -1];
+        p2DiagLastFour = [-1, -1, -1, -1];
         let moveRow, moveCol;
 
         // for the right-down diagonals across the top
@@ -114,6 +188,14 @@ function evaluatePosition(){
                 p2DiagStreak++;
             }
 
+            // adjust the lists of the last four elements seen
+            for (let i = 3; i >= 1; i--) {
+                p1DiagLastFour[i] = p1DiagLastFour[i - 1];
+                p2DiagLastFour[i] = p2DiagLastFour[i - 1];
+            }
+            p1DiagLastFour[0] = gameBoard[moveRow][moveCol];
+            p2DiagLastFour[0] = gameBoard[moveRow][moveCol];
+
             // adjust the col depending on which direction we are looking
             if (i < 6) {
                 moveCol++;
@@ -123,16 +205,76 @@ function evaluatePosition(){
             }
             moveRow ++;
 
-            // if we have a streak of 4 or more, add to the score
+            // if we have a streak of 4 or more, add to the score and check for how many pieces in the last four we have already seen
+            let counters;
             if (p1DiagStreak >= 4){
                 p1Score ++;
+                counters = incrementScores(countLastFour(1, p1DiagLastFour), p1Singles, p1Doubles, p1Triples, p1Win);
+                p2Singles = counters[0];
+                p2Doubles = counters[1];
+                p2Triples = counters[2];
+                p2Win = counters[3];
             }
             if (p2DiagStreak >= 4){
                 p2Score ++;
+                counters = incrementScores(countLastFour(1, p2DiagLastFour), p2Singles, p2Doubles, p2Triples, p2Win);
+                p2Singles = counters[0];
+                p2Doubles = counters[1];
+                p2Triples = counters[2];
+                p2Win = counters[3];
             }
         }
     }
 
-    // returns a score from 10 to -10, 10 being p1 wins, -10 being p2 wins
-    return Math.round(p1Score-p2Score)/10;
+    // returns an array of the calculated scores
+    // array format is: [p1Score, p2Score, p1Singles, p2Singles...., p2Win]
+    return [p1Score, p2Score, p1Singles, p2Singles, p1Doubles, p2Doubles, p1Triples, p2Triples, p1Win, p2Win];
+}
+
+/**
+ * Counts the number of times a player's pieces are in the last 4 pieces looked at
+ * @param player the integer for player 1 or 2
+ * @param lastFour the array of the last 4 pieces seen on the board
+ * @return {number} the number of the player's pieces which were seen in the last 4 given
+ */
+function countLastFour(player, lastFour){
+    let count = 0;
+    for (let i = 0; i < 4; i++){
+        if(lastFour[i] === player){
+            count ++;
+        }
+    }
+
+    return count;
+}
+
+/**
+ * increments the correct counter based on the number of resents seen and returns all the counters after modifying one
+ * @param num the number of pieces seen in the last four
+ * @param singles the number of singles so far for this player
+ * @param doubles the number of doubles so far for this player
+ * @param triples the number of triples so far for this player
+ * @param win 1 if the player has had a win, 0 otherwise
+ * @return {(*|number)[]} an array with all the counters
+ */
+function incrementScores(num, singles, doubles, triples, win){
+    if (num > 0){
+        if (num > 1){
+            if (num > 2){
+                if (num > 3){
+                    win = 1;
+                }
+                else {
+                    triples++;
+                }
+            }
+            else {
+                doubles++;
+            }
+        }
+        else {
+            singles++;
+        }
+    }
+    return [singles, doubles, triples, win];
 }
