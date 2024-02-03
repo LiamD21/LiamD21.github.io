19e601db38f65fc36c38f548d262ccda88f33342
LiamD21 <58191445+LiamD21@users.noreply.github.com>
2024-02-02 23:26:26 -0600

    Created connect four opponent basic one level search for the best move

--- src/ConnectFour/modules/cFourOpponent.js
+++ src/ConnectFour/modules/cFourOpponent.js
@@ -1,6 +1,5 @@
 // importing
-import {GameBoard} from "./gameBoardClass";
-import {getEvalScore} from "./cFourEvaluator";
+import {cFourEvaluator} from "./cFourEvaluator";
 
 export class cFourOpponent{
     // define private class attributes
@@ -10,8 +9,13 @@ export class cFourOpponent{
 
     constructor(state) {
         this.#state = state;
+        this.#bestEval = 11;
     }
 
+    /**
+     * Find all possible actions from this state
+     * @return {*[]}
+     */
     #actions(){
         let actns = [];
         for (let i = 0; i < 8; i++){
@@ -22,12 +26,32 @@ export class cFourOpponent{
         return actns;
     }
 
+    /**
+     * search all possible actions in this state to find the best one
+     */
     #searchLevel(){
         let actns = this.#actions();
         for (let i = 0; i < actns.length; i++){
             let stateCpy = this.#state.copyBoard();
-            stateCpy.playPiece(i);
+            let evaluator = new cFourEvaluator();
 
+            stateCpy.playPiece(i);
+            evaluator.updateGameBoard(stateCpy.getGameState(), stateCpy.getTurn());
+
+            let score = evaluator.getEvalScore();
+            if (score < this.#bestEval){
+                this.#bestEval = score;
+                this.#bestMove = i;
+            }
         }
+    }
+
+    /**
+     * performs the search and returns the best move to make
+     * @return {*}
+     */
+    doSearch(){
+        this.#searchLevel();
+        return this.#bestMove
     }
 }
